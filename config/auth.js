7352baff849f235f928e79e4d74ba7586fb3a1de
Neardanger <steven.magadan@hotmail.com>
2016-10-06 11:43:29 -0700

    added material design icons

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'facebook':{
+        'clientID':'1131143190287421',
+        'clientSecret':'0ccccf2622eea557d18857b9db3651b3',
+        'callbackURL':'http://giphtur.herokuapp.com/auth/facebook/callback',
+        'profileFields':['emails','displayName']
+
+    }
+}
