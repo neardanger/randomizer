9c6e156aabb744de33846bb095be36e105939cc2
Neardanger <steven.magadan@hotmail.com>
2016-10-10 14:19:30 -0700

    added signup form

--- public/js/init.js
+++ public/js/init.js
@@ -8,23 +8,6 @@
     selectYears: 200 // Creates a dropdown of 15 years to control year
     });
 
-function checkDate() {
-	if ($('#buildDate').val() == '') {
-    $('#buildDate').addClass('invalid')
-  } else {
-    $('#buildDate').removeClass('invalid')
-  }
-}
-
-$('form').submit(function() {
-  checkDate();
-  return false;
-});
-
-$('#buildDate').change(function() {
-  checkDate();
-});
-
     $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
