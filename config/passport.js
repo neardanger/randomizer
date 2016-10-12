1e5df85390c182b9bb1a2dcb2456035e79718b51
Neardanger <steven.magadan@hotmail.com>
2016-10-11 17:05:08 -0700

    fixed logout in routes

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,28 @@
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    FacebookStrategy = require('passport-facebook').Strategy, 
     User = require('../models/User.js') 
 
 
+
+
+//The user object turns into a cookie here
     passport.serializeUser(function(user,done){
         done(null,user.id)
     }) 
 
+//The cookie goes into usser object
     passport.deserializeUser(function(id,done){
         User.findById(id,function(err,user){
             done(err,user)
         })
     })
-    //Signing in
+    //create a user
     passport.use('local-signup', new LocalStrategy({
 
         usernameField:'email',
         passwordField:'password',
-        passReqToCallback: true}, 
+        passReqToCallback: true
+    }, 
         
         function(req,email,password,done){
 
@@ -43,13 +47,15 @@ var passport = require('passport'),
     passport.use('local-login', new LocalStrategy({
         usernameField:'email',
         passwordField: 'password',
-        passReqToCallback: true}, 
+        passReqToCallback: true
+    }, 
         
         function(req,email,password,done){
         User.findOne({'local.email':email}, function(err,user){
             if(err) return done(err)
             if(!user) return done (null,false,req.flash('loginMessage','That user does not exist!'))
             if(!user.validPassword(password)) return done(null,false,req.flash('loginMessage','That is not the correct password'))
+            return done(null,user)
         })
     }))
 
