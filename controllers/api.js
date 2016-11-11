362124f7f845fceca03a36ee1c6fb9498033dd35
steven magadan <steven.magadan@hotmail.com>
2016-11-11 14:08:41 -0800

    fixed api routes

--- controllers/api.js
+++ controllers/api.js
@@ -1,12 +1,15 @@
 var Path = require('path');
 var request = require('request');
-var apiTems = ["2636","3920","3920_582507_583874", "4044_133012_1045881",
+
+
+var products = ["2636","3920","3920_582507_583874", "4044_133012_1045881",
+"3944", "4171","2637_667479", "4171_1111647", "5438_426265_1032639",
   "4104", "4171_4191", "4096","4125_4161","1085632_1229464",
   "4171_1015079"]
 
   var randomizer = function()
   
-  {return (Math.floor(Math.random() * (apiTems.length - 0)) + 0)} 
+  {return (Math.floor(Math.random() * (products.length - 0)) + 0)} 
 
 
 
@@ -16,28 +19,28 @@ var apiTems = ["2636","3920","3920_582507_583874", "4044_133012_1045881",
 module.exports = {
 
     show: function(req,res){
-        var apiTemsArray = []
+        var productsArray = []
         for(var i = 0;i < 3; i++){
-            apiTemsArray[i] = "http://api.walmartlabs.com/v1/paginated/items?format=json&category" + apiTems[randomizer()] +"&apiKey="+"9nh57qxqy5gu6sc4xtewsydr"
+            productsArray[i] = "http://api.walmartlabs.com/v1/paginated/items?format=json&category=" + products[randomizer()] +"&apiKey=9nh57qxqy5gu6sc4xtewsydr"
         }
     var data = []
 
-    request({url: apiTemsArray[0],json:true}, function(error,response,body){
+    request({url: productsArray[0],json:true}, function(error,response,body){
         var randomizer2 = function (){return (Math.floor(Math.random() * (body.items.length - 0)) + 0)}
-        var vapeNation =  body.items[randomizer2()]
-        data.push(vapeNation)
+        var getItem =  body.items[randomizer2()]
+        data.push(getItem)
     
 
-     request({url: apiTemsArray[1],json:true}, function(error,response,body){
+     request({url: productsArray[1],json:true}, function(error,response,body){
         var randomizer2 = function  (){return (Math.floor(Math.random() * (body.items.length - 0)) + 0)}
-        var vapeNation =  body.items[randomizer2()]
-        data.push(vapeNation)
+        var getItem =  body.items[randomizer2()]
+        data.push(getItem)
     
 
-     request({url: apiTemsArray[2],json:true}, function(error,response,body){
+     request({url: productsArray[2],json:true}, function(error,response,body){
         var randomizer2 = function (){return (Math.floor(Math.random() * (body.items.length - 0)) + 0)}
-        var vapeNation =  body.items[randomizer2()]
-        data.push(vapeNation)
+        var getItem =  body.items[randomizer2()]
+        data.push(getItem)
 
         res.json(data)
             })
