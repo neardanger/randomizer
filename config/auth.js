c6bb9dbd4a2947a83918edf06c121d643c104ca4
steven magadan <steven.magadan@hotmail.com>
2016-11-12 17:57:09 -0800

    changed

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebook':{
         'clientID':'1131143190287421',
         'clientSecret':'0ccccf2622eea557d18857b9db3651b3',
-        'callbackURL':'https://localhost:8000/auth/facebook/callback',
+        'callbackURL':'https://giphtur.herokuapp.com/auth/facebook/callback',
         'profileFields':['emails','displayName']
 
     }
