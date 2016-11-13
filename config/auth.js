fab8f7f5a883fe38d1b66d792d51abd5edbaa87e
steven magadan <steven.magadan@hotmail.com>
2016-11-12 17:26:11 -0800

    routes change

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebook':{
         'clientID':'1131143190287421',
         'clientSecret':'0ccccf2622eea557d18857b9db3651b3',
-        'callbackURL':'http://giphtur.herokuapp.com/auth/facebook/callback',
+        'callbackURL':'https://giphtur.herokuapp.com/auth/facebook/callback',
         'profileFields':['emails','displayName']
 
     }
