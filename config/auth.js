bdc73e8d11514d275d9b1740c68f4f3956644369
steven magadan <steven.magadan@hotmail.com>
2016-11-15 16:06:06 -0800

    added button

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
         'clientID':'1615737972055027',
         'clientSecret':'92769e9831761813d99fcb30e7504ad3',
         'callbackURL':'https://giphtur.herokuapp.com/auth/facebook/callback',
-        'profileFields':['emails','displayName','photos']
+        'profileFields':['emails','displayName']
 
     }
 }
